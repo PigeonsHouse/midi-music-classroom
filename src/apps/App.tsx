c67d654f250fa98d4464721b1877392f21c9251a
PigeonsHouse <bullbull1230@gmail.com>
2025-05-06 19:30:36 +0900

    update: バグ修正

--- src/apps/App.tsx
+++ src/apps/App.tsx
@@ -23,21 +23,23 @@ export const App = () => {
   const updatePushingKeyNumbers = useCallback(
     (newNoteNumber: number, isOn: boolean) => {
       newNoteNumber += transposeScale;
-      if (isOn) {
-        setPushingKeyNumbers((pushedKeyNumbers) =>
-          pushingKeyNumbers.includes(newNoteNumber)
-            ? pushedKeyNumbers
-            : pushedKeyNumbers.concat(newNoteNumber),
-        );
-        beepNote(newNoteNumber);
-      } else {
-        setPushingKeyNumbers((pushedKeyNumbers) => {
-          const index = pushedKeyNumbers.indexOf(newNoteNumber);
-          return index === -1
-            ? pushedKeyNumbers
-            : pushedKeyNumbers.toSpliced(index, 1);
-        });
-      }
+      const numberUpdateCallback = isOn
+        ? (pushedKeyNumbers: number[]) => {
+            const isExist = pushingKeyNumbers.includes(newNoteNumber);
+            if (isExist) {
+              return pushedKeyNumbers;
+            } else {
+              beepNote(newNoteNumber);
+              return pushedKeyNumbers.concat(newNoteNumber);
+            }
+          }
+        : (pushedKeyNumbers: number[]) => {
+            const index = pushedKeyNumbers.indexOf(newNoteNumber);
+            return index === -1
+              ? pushedKeyNumbers
+              : pushedKeyNumbers.toSpliced(index, 1);
+          };
+      setPushingKeyNumbers(numberUpdateCallback);
     },
     [pushingKeyNumbers, transposeScale],
   );
